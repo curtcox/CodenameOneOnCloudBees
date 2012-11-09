bcc0c286aaaa84ff06815862a4f2df502b61dc5a
Curt Cox <curtcox@gmail.com>
2012-11-09 07:02:26 -0600

    Added CodenameOne Hello World files

---
+++ src/com/mycompany/myapp/MyApplication.java
@@ -0,0 +1,41 @@
+package com.mycompany.myapp;
+
+
+import com.codename1.ui.Display;
+import com.codename1.ui.Form;
+import com.codename1.ui.Label;
+import com.codename1.ui.plaf.UIManager;
+import com.codename1.ui.util.Resources;
+import java.io.IOException;
+
+public class MyApplication {
+
+    private Form current;
+
+    public void init(Object context) {
+        try{
+            Resources theme = Resources.openLayered("/theme");
+            UIManager.getInstance().setThemeProps(theme.getTheme(theme.getThemeResourceNames()[0]));
+       }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void start() {
+        if(current != null){
+            current.show();
+            return;
+        }
+        Form hi = new Form("Hi World");
+        hi.addComponent(new Label("Hi World"));
+        hi.show();
+    }
+
+    public void stop() {
+        current = Display.getInstance().getCurrent();
+    }
+    
+    public void destroy() {
+    }
+
+}
